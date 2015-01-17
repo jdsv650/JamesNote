1126920453665f53604ae3df362f76803e1b7321
James <jdsv650@yahoo.com>
2015-01-17 09:17:34 -0500

    pre advanced class

--- JamesNote/NotesTableViewController.swift
+++ JamesNote/NotesTableViewController.swift
@@ -69,15 +69,6 @@ class NotesTableViewController: UITableViewController {
         
     }
     
-    
-    
-    // Override to support conditional editing of the table view.
-//    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-//        // Return NO if you do not want the specified item to be editable.
-//        return true
-//    }
-
-    
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == UITableViewCellEditingStyle.Delete {
@@ -90,35 +81,7 @@ class NotesTableViewController: UITableViewController {
     }
 
     
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
-    
-    
-    
+    // unwind segue 
     @IBAction func saveNote(segue: UIStoryboardSegue)
     {
         if let indexPath = tableView.indexPathForSelectedRow()  {
@@ -134,12 +97,7 @@ class NotesTableViewController: UITableViewController {
             let lastRow = NSIndexPath(forItem: noteStore.count() - 1, inSection: 0)
             
             tableView.insertRowsAtIndexPaths([lastRow], withRowAnimation: UITableViewRowAnimation.Automatic)
-            
-
         }
-        
     }
-    
-    
 
 }
